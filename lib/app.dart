77dfa38a2a33bb835d9139c9aedf9ab57b0ef88c
Somadina <kingswatter@gmail.com>
2025-03-12 12:01:58 +0100

    gotten everything to work

---
+++ lib/app.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'screens/auth/login_screen.dart';
+import 'screens/auth/register_screen.dart';
+import 'screens/home_screen.dart';
+import 'screens/splash_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SplashScreen(),
+      routes: {
+        '/login': (context) => LoginScreen(),
+        '/register': (context) => RegisterScreen(),
+        '/home': (context) => HomeScreen(),
+      },
+    );
+  }
+}
